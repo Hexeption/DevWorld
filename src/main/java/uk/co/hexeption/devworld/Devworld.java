ea88d14992feeec8eae649bce431b8bf88acb94a
Hexeption <minecraftfun201@gmail.com>
2020-04-23 16:56:25 +0100

    Updated to 20w17A

--- src/main/java/uk/co/hexeption/devworld/Devworld.java
+++ src/main/java/uk/co/hexeption/devworld/Devworld.java
@@ -1,19 +1,21 @@
 package uk.co.hexeption.devworld;
 
 import java.io.File;
+import java.io.IOException;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.datafixer.NbtOps;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.Difficulty;
 import net.minecraft.world.GameMode;
-import net.minecraft.world.WorldSaveHandler;
 import net.minecraft.world.gen.chunk.FlatChunkGeneratorConfig;
 import net.minecraft.world.level.LevelGeneratorType;
 import net.minecraft.world.level.LevelInfo;
 import net.minecraft.world.level.LevelProperties;
 import net.minecraft.world.level.storage.LevelStorage;
+import net.minecraft.world.level.storage.LevelStorage.Session;
 
 @Environment(EnvType.CLIENT)
 public class Devworld implements ModInitializer {
@@ -33,63 +35,78 @@ public class Devworld implements ModInitializer {
 
         FlatChunkGeneratorConfig flatChunkGeneratorConfig = FlatChunkGeneratorConfig.fromString("minecraft:bedrock,3*minecraft:stone,52*minecraft:sandstone;minecraft:desert;");
 
-        LevelInfo levelInfo = new LevelInfo(0, GameMode.CREATIVE, false, false, LevelGeneratorType.FLAT.loadOptions(flatChunkGeneratorConfig.toDynamic(NbtOps.INSTANCE)));
+        LevelInfo levelInfo = new LevelInfo(worldName, 0, GameMode.CREATIVE, false, false, Difficulty.NORMAL,
+            LevelGeneratorType.FLAT.loadOptions(flatChunkGeneratorConfig.toDynamic(NbtOps.INSTANCE)));
 
         File gameDir = MinecraftClient.getInstance().runDirectory;
         LevelStorage levelStorage = new LevelStorage(gameDir.toPath().resolve("saves"), gameDir.toPath().resolve("backups"), null);
-        WorldSaveHandler worldSaveHandler = levelStorage.createSaveHandler(worldName, null);
+        try {
+            Session session = levelStorage.createSession(worldName);
 
-        CompoundTag worldData = new CompoundTag();
+            CompoundTag worldData = new CompoundTag();
 
-        // Spawn Location
-        worldData.putInt("SpawnX", 0);
-        worldData.putInt("SpawnY", 60);
-        worldData.putInt("SpawnZ", 0);
+            // Spawn Location
+            worldData.putInt("SpawnX", 0);
+            worldData.putInt("SpawnY", 55);
+            worldData.putInt("SpawnZ", 0);
 
-        // World Generator
-        worldData.putString("generatorName", "flat");
-        worldData.put("generatorOptions", flatChunkGeneratorConfig.toDynamic(NbtOps.INSTANCE).getValue());
+            worldData.putInt("Difficulty", 2);
 
-        // Cheat Mode
-        worldData.putInt("GameType", GameMode.CREATIVE.getId());
-        worldData.putBoolean("allowCommands", true);
+            // World Generator
+            worldData.putString("generatorName", "flat");
+            worldData.put("generatorOptions", flatChunkGeneratorConfig.toDynamic(NbtOps.INSTANCE).getValue());
 
-        // World Initialized
-        worldData.putBoolean("initialized", true);
+            // Cheat Mode
+            worldData.putInt("GameType", GameMode.CREATIVE.getId());
+            worldData.putBoolean("allowCommands", true);
 
-        // Set Day
-        worldData.putLong("Time", 6000);
-        worldData.putLong("DayTime", 6000);
+            // World Initialized
+            worldData.putBoolean("initialized", true);
 
-        // Game Rules
-        CompoundTag gamerules = new CompoundTag();
-        gamerules.putString("doWeatherCycle", "false");
-        gamerules.putString("doDaylightCycle", "false");
-        worldData.put("GameRules", gamerules);
+            // Set Day
+            worldData.putLong("Time", 6000);
+            worldData.putLong("DayTime", 6000);
 
-        // Save World
-        LevelProperties levelProperties = new LevelProperties(worldData, MinecraftClient.getInstance().getDataFixer(), 15, null);
-        worldSaveHandler.saveWorld(levelProperties);
+            // Game Rules
+            CompoundTag gamerules = new CompoundTag();
+            gamerules.putString("doWeatherCycle", "false");
+            gamerules.putString("doDaylightCycle", "false");
+            worldData.put("GameRules", gamerules);
+
+            // Save World
+            LevelProperties levelProperties = new LevelProperties(worldData, MinecraftClient.getInstance().getDataFixer(), 16, null);
+
+            session.method_27426(levelProperties, worldData);
+            session.save(worldName);
+            session.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         // Start the World
-        MinecraftClient.getInstance().startIntegratedServer(worldName, worldName, levelInfo);
-
+        MinecraftClient.getInstance().startIntegratedServer(worldName, levelInfo);
     }
 
     public void deleteWorld() {
         LevelStorage levelStorage = MinecraftClient.getInstance().getLevelStorage();
-        levelStorage.deleteLevel(worldName);
+        try {
+            Session session = levelStorage.createSession(worldName);
+            session.deleteSessionLock();
+            session.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public boolean loadWorld() {
         if (MinecraftClient.getInstance().getLevelStorage().levelExists(worldName)) {
-            MinecraftClient.getInstance().startIntegratedServer(worldName, worldName, null);
+            MinecraftClient.getInstance().startIntegratedServer(worldName, null);
             return true;
         }
         return false;
     }
 
     public boolean saveExist() {
-        return MinecraftClient.getInstance().getLevelStorage().getLevelProperties(worldName) != null;
+        return MinecraftClient.getInstance().getLevelStorage().levelExists(worldName);
     }
 }
--- src/main/java/uk/co/hexeption/devworld/mixin/MixinTitleScreen.java
+++ src/main/java/uk/co/hexeption/devworld/mixin/MixinTitleScreen.java
@@ -4,8 +4,10 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.resource.language.I18n;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Util;
 import net.minecraft.util.math.MathHelper;
 import org.lwjgl.glfw.GLFW;
@@ -47,17 +49,13 @@ public class MixinTitleScreen extends Screen {
         int buttonY = height / 4 + 48;
         int buttonX = width / 2 + 104;
 
-        buttonCreate = new ButtonWidget(buttonX, buttonY, 84, 20, I18n.translate("menu.new.devworld", new Object[0]), button -> {
-            Devworld.INSTANCE.createWorld();
-        });
+        buttonCreate = new ButtonWidget(buttonX, buttonY, 84, 20, new TranslatableText("menu.new.devworld", new Object[0]), button -> Devworld.INSTANCE.createWorld());
 
-        buttonLoad = new ButtonWidget(buttonX, buttonY, 40, 20, I18n.translate("menu.load.devworld", new Object[0]), button -> {
-            Devworld.INSTANCE.loadWorld();
-        });
+        buttonLoad = new ButtonWidget(buttonX, buttonY, 40, 20, new TranslatableText("menu.load.devworld", new Object[0]), button -> Devworld.INSTANCE.loadWorld());
 
         buttonX += 44;
 
-        buttonDelete = new ButtonWidget(buttonX, buttonY, 40, 20, I18n.translate("menu.delete.devworld", new Object[0]), button -> {
+        buttonDelete = new ButtonWidget(buttonX, buttonY, 40, 20, new TranslatableText("menu.delete.devworld", new Object[0]), button -> {
             Devworld.INSTANCE.deleteWorld();
             keyShiftCount = 0;
         });
@@ -86,20 +84,20 @@ public class MixinTitleScreen extends Screen {
     }
 
     @Inject(method = "render", at = @At("RETURN"))
-    private void render(int mouseX, int mouseY, float delta, CallbackInfo ci) {
+    private void render(MatrixStack matrixStack, int mouseY, int i, float f, CallbackInfo ci) {
         int textY = height / 4 + 20;
         int textX = width / 2 + 146;
 
-        float f = this.doBackgroundFade ? (float) (Util.getMeasuringTimeMs() - this.backgroundFadeStart) / 1000.0F : 1.0F;
-        float g = this.doBackgroundFade ? MathHelper.clamp(f - 1.0F, 0.0F, 1.0F) : 1.0F;
+        float ffade = this.doBackgroundFade ? (float) (Util.getMeasuringTimeMs() - this.backgroundFadeStart) / 1000.0F : 1.0F;
+        float g = this.doBackgroundFade ? MathHelper.clamp(ffade - 1.0F, 0.0F, 1.0F) : 1.0F;
         int l = MathHelper.ceil(g * 255.0F) << 24;
         if ((l & -67108864) != 0) {
 
-            drawCenteredString(MinecraftClient.getInstance().textRenderer, "Dev World", textX, textY, 16777215 | l);
+            drawCenteredString(matrixStack, MinecraftClient.getInstance().textRenderer, "Dev World", textX, textY, 16777215 | l);
         }
 
         if (buttonDelete.isHovered() && !buttonDelete.active) {
-            renderTooltip("Press [Left Shift] 2 times to enable delete", mouseX, mouseY);
+            renderTooltip(matrixStack, new LiteralText("Press [Left Shift] 2 times to enable delete"), mouseY, i);
         }
 
         if (!Devworld.INSTANCE.saveExist()) {
